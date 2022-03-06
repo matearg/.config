92abcbf62c1f19051ad5df2ec219e6af992a1648
Mateo <mateoarcidiacono@gmail.com>
2022-03-06 20:08:00 -0300

    minor changes

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -21,8 +21,8 @@ Set-PSReadLineOption -EditMode Windows
 Set-PSReadLineOption -PredictionViewStyle ListView
 
 # Prompt
-Clear-Host
+# Clear-Host
 # neofetch.ps1
-# Invoke-Expression (&starship init powershell)
-oh-my-posh --init --shell pwsh --config ~\.config\powershell\takuya.omp.json | Invoke-Expression
+Invoke-Expression (&starship init powershell)
+# oh-my-posh --init --shell pwsh --config ~\.config\powershell\takuya.omp.json | Invoke-Expression
 # oh-my-posh --init --shell pwsh --config ~\scoop\apps\oh-my-posh\current\themes\atomicBit.omp.json | Invoke-Expression
