7e78f18efb822d2056b484cd0492d4954befe62a
matearg <mateoarcidiacono@gmail.com>
2022-03-10 00:05:26 -0300

    minnor changes

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -23,9 +23,6 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 # Prompt
 Clear-Host
 # neofetch
-function Invoke-Starship-PreCommand {
-    $host.ui.Write("🚀 ")
-}
 Invoke-Expression (&starship init powershell)
 # oh-my-posh --init --shell pwsh --config ~\.config\powershell\takuya.omp.json | Invoke-Expression
 # oh-my-posh --init --shell pwsh --config ~\scoop\apps\oh-my-posh\current\themes\atomicBit.omp.json | Invoke-Expression
