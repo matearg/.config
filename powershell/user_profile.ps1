3bddb35d5427044782e79cf1d311df5ceeee5504
matearg <mateoarcidiacono@gmail.com>
2022-03-10 20:30:02 -0300

    better syntax

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -23,6 +23,6 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 # Prompt
 Clear-Host
 # neofetch
-Invoke-Expression (&starship init powershell)
-# oh-my-posh --init --shell pwsh --config ~\.config\powershell\takuya.omp.json | Invoke-Expression
+# Invoke-Expression (&starship init powershell)
+oh-my-posh --init --shell pwsh --config ~\.config\powershell\truepowerline.omp.json | Invoke-Expression
 # oh-my-posh --init --shell pwsh --config ~\scoop\apps\oh-my-posh\current\themes\atomicBit.omp.json | Invoke-Expression
