e59f3da869a2ff0f7efa54850f27aa1f51f5cb38
matearg <mateoarcidiacono@gmail.com>
2022-04-11 22:19:12 -0300

    Changed some powershell settings

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -3,26 +3,28 @@
 
 # Imported Modules
 Import-Module Terminal-Icons
-# Import-Module posh-git
-# Import-Module PSReadLine
+Import-Module posh-git
+Import-Module PSReadLine
 
 # Alias
 Set-Alias ll ls
-Set-Alias c clear
-Set-Alias wh where.exe
 Set-Alias g git
-Set-Alias n nvim
-Set-Alias v nvim
 Set-Alias vim nvim
 
 # PSReadLine
-# Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-# Set-PSReadLineOption -EditMode Windows
-# Set-PSReadLineOption -PredictionViewStyle InLineView
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -PredictionViewStyle List
 
 # Prompt
 Clear-Host
 # neofetch
-Invoke-Expression (&starship init powershell)
-# oh-my-posh --init --shell pwsh --config ~\.config\powershell\mytheme.omp.json | Invoke-Expression
-# oh-my-posh --init --shell pwsh --config ~\scoop\apps\oh-my-posh\current\themes\half-life.omp.json | Invoke-Expression
+# Invoke-Expression (&starship init powershell)
+oh-my-posh --init --shell pwsh --config ~\.config\powershell\mytheme.omp.json | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config ~\scoop\apps\oh-my-posh\current\themes\clean-detailed.omp.json | Invoke-Expression
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
