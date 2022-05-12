b7a1a1edba35fec000155d0b1ef15f2c49773c4a
matearg <mateoarcidiacono@gmail.com>
2022-05-11 21:47:46 -0300

    Changed ohmyposh default theme

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -24,7 +24,7 @@ Invoke-Expression (&starship init powershell)
 
 # Oh my Posh
 # oh-my-posh --init --shell pwsh --config ~\.config\powershell\mytheme.omp.json | Invoke-Expression
-# oh-my-posh --init --shell pwsh --config ~\scoop\apps\oh-my-posh\current\themes\M365Princess.omp.json | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config ~\scoop\apps\oh-my-posh\current\themes\takuya.omp.json | Invoke-Expression
 
 # Utilities
 function which ($command) {
